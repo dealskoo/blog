84d9cd0995597469b301f4e7c4dee6e5260a7f66
jabber <2898117012@qq.com>
2022-03-02 11:27:06 +0800

    init

--- src/Models/Blog.php
+++ src/Models/Blog.php
@@ -2,10 +2,38 @@
 
 namespace Dealskoo\Blog\Models;
 
+use Dealskoo\Comment\Traits\Commentable;
+use Dealskoo\Country\Traits\HasCountry;
+use Dealskoo\Tag\Traits\Taggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Blog extends Model
 {
-    use HasFactory;
+    use HasFactory, HasCountry, Taggable, Commentable;
+
+    protected $appends = [
+        'cover_url'
+    ];
+
+    protected $fillable = [
+        'slug',
+        'title',
+        'cover',
+        'content',
+        'published_at',
+        'can_comment',
+        'country_id'
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+        'can_comment' => 'boolean',
+    ];
+
+    public function getCoverUrlAttribute()
+    {
+        return empty($this->cover) ? '' : Storage::url($this->cover);
+    }
 }
