4a40a6c5343628603238c4a403256b42c451b60d
jabber <2898117012@qq.com>
2022-03-28 18:46:31 +0800

    init

--- resources/views/admin/blog/create.blade.php
+++ resources/views/admin/blog/create.blade.php
@@ -34,7 +34,7 @@
                             </div>
                         @endif
                         <div class="row">
-                            <div class="col-md-8">
+                            <div class="col-md-10">
                                 <div class="row">
                                     <div class="col-12 mb-3">
                                         <input type="text" class="form-control" id="title" name="title" required
@@ -54,7 +54,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-4">
+                            <div class="col-md-2">
                                 <div class="row">
                                     <div class="col-12 mb-3">
                                         <div class="cover-box">
--- resources/views/admin/blog/edit.blade.php
+++ resources/views/admin/blog/edit.blade.php
@@ -35,7 +35,7 @@
                             </div>
                         @endif
                         <div class="row">
-                            <div class="col-md-8">
+                            <div class="col-md-10">
                                 <div class="row">
                                     <div class="col-12 mb-3">
                                         <input type="text" class="form-control" id="title" name="title" required
@@ -55,7 +55,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-4">
+                            <div class="col-md-2">
                                 <div class="row">
                                     <div class="col-12 mb-3">
                                         <div class="cover-box">
